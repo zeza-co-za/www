2a097469cad76126c85296bc5a1c56587cf65e9b
Raydo M <126121348+burnt-exe@users.noreply.github.com>
2026-07-04 14:29:05 +0200

    Add lead magnet download helper

---
+++ assets/lead-magnet-download.js
@@ -0,0 +1,7 @@
+const ZEZA_LEAD_MAGNET_URL = '/downloads/digital-marketing-made-easy-starter-guide.pdf';
+
+function redirectToLeadMagnet() {
+  window.location.href = ZEZA_LEAD_MAGNET_URL;
+}
+
+// Production use: call redirectToLeadMagnet() after the visitor submits a valid email address.
